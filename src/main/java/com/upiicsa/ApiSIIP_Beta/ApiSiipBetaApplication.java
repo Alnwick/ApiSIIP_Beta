e17fbf3ef2b6b8d1c5d23c51e1c2e5346d7f36ba
Alnwick <Alnwick0809@outlook.com>
2025-10-14 13:24:24 -0600

    Inicializacion de proyecto

---
+++ src/main/java/com/upiicsa/ApiSIIP_Beta/ApiSiipBetaApplication.java
@@ -0,0 +1,13 @@
+package com.upiicsa.ApiSIIP_Beta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSiipBetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSiipBetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upiicsa/ApiSIIP_Beta/ApiSiipBetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upiicsa.ApiSIIP_Beta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSiipBetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
