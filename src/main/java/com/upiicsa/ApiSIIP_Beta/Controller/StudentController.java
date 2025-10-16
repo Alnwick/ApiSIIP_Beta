4532289038ff6df262e366618b792ec9fd86c494
Alnwick <Alnwick0809@outlook.com>
2025-10-16 15:20:12 -0600

    Metodo para que el estudiante guarde un documento.

--- src/main/java/com/upiicsa/ApiSIIP_Beta/Controller/StudentController.java
+++ src/main/java/com/upiicsa/ApiSIIP_Beta/Controller/StudentController.java
@@ -1,20 +1,23 @@
 package com.upiicsa.ApiSIIP_Beta.Controller;
 
+import com.upiicsa.ApiSIIP_Beta.Dto.Document.UploadDocDto;
 import com.upiicsa.ApiSIIP_Beta.Dto.Email.EmailConfirmationDto;
 import com.upiicsa.ApiSIIP_Beta.Dto.Student.ResponseRegStudentDto;
 import com.upiicsa.ApiSIIP_Beta.Dto.Student.StudentRegistrationDto;
 import com.upiicsa.ApiSIIP_Beta.Model.Student;
+import com.upiicsa.ApiSIIP_Beta.Service.DocumentService;
 import com.upiicsa.ApiSIIP_Beta.Service.EmailVerificationService;
 import com.upiicsa.ApiSIIP_Beta.Service.StudentService;
+import com.upiicsa.ApiSIIP_Beta.Utils.AuthHelper;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.IOException;
 import java.net.URI;
 
 @RestController
@@ -26,6 +29,9 @@ public class StudentController {
 
     @Autowired
     private EmailVerificationService verificationService;
+
+    @Autowired
+    private DocumentService documentService;
 
     @PostMapping("/register")
     public ResponseEntity<ResponseRegStudentDto> registerUser(@RequestBody @Valid StudentRegistrationDto registrationDto) {
@@ -46,6 +52,25 @@ public class StudentController {
             return ResponseEntity.ok().build();
         } catch (IllegalArgumentException e){
             return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+    @PutMapping("/upload-document")
+    @PreAuthorize("hasAnyRole('STUDENT')")
+    public ResponseEntity<String> uploadDocument (@ModelAttribute UploadDocDto uploadDoc){
+        MultipartFile file = uploadDoc.file();
+        String typeDocument = uploadDoc.typeDocument();
+        Long idUser = AuthHelper.getAuthenticatedUserId();
+
+        if(file.isEmpty()){
+            return ResponseEntity.badRequest().body("Please select a file");
+        }
+
+        try {
+            String fileUrl = documentService.saveFile(file, typeDocument, idUser);
+            return ResponseEntity.ok("Archive saved successfully. URL: " + fileUrl);
+        } catch (IOException e){
+            return ResponseEntity.internalServerError().body("Error saving file: " + e.getMessage());
         }
     }
 }
