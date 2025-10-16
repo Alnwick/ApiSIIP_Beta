ad5664d5212298d4636e436ac49f2ba1ddd114c9
Alnwick <Alnwick0809@outlook.com>
2025-10-16 15:14:18 -0600

    Clases de apoyo

--- src/main/java/com/upiicsa/ApiSIIP_Beta/Utils/AuthHelper.java
+++ src/main/java/com/upiicsa/ApiSIIP_Beta/Utils/AuthHelper.java
@@ -1,4 +1,27 @@
 package com.upiicsa.ApiSIIP_Beta.Utils;
 
+import com.upiicsa.ApiSIIP_Beta.Model.UserSIIP;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 public class AuthHelper {
+
+    public static Long getAuthenticatedUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated() ||
+                authentication.getPrincipal().equals("anonymousUser")) {
+
+            // If the endpoint is protected, this indicates access not authorize
+            throw new IllegalStateException("Not authorized for this operation.");
+        }
+
+        Object principal = authentication.getPrincipal();
+
+        if (principal instanceof UserSIIP user) {
+            return user.getId();
+        }
+
+        throw new IllegalStateException("The principal is not an instance of authenticated user.");
+    }
 }
--- src/main/java/com/upiicsa/ApiSIIP_Beta/Utils/WebConfig.java
+++ src/main/java/com/upiicsa/ApiSIIP_Beta/Utils/WebConfig.java
@@ -1,4 +1,15 @@
 package com.upiicsa.ApiSIIP_Beta.Utils;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:./uploads/");
+    }
 }
