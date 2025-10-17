c333a56919adeaf9997b91d7bde7d83f669453f9
Alnwick <Alnwick0809@outlook.com>
2025-10-17 15:44:44 -0600

    Se agrego import.

--- src/main/java/com/upiicsa/ApiSIIP_Beta/Model/DocumentHistory.java
+++ src/main/java/com/upiicsa/ApiSIIP_Beta/Model/DocumentHistory.java
@@ -2,10 +2,7 @@ package com.upiicsa.ApiSIIP_Beta.Model;
 
 import com.upiicsa.ApiSIIP_Beta.Model.Enum.StateDocument;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
