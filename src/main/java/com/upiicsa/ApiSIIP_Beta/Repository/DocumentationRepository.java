a0621618ea467bac7d70658f1f50c0785922dacd
Alnwick <Alnwick0809@outlook.com>
2025-10-16 18:14:58 -0600

    Service -> Se agrego el metodo para obtener la documentacion de un alumno. Repository -> Consulta para obtener la documentacion por Id.

--- src/main/java/com/upiicsa/ApiSIIP_Beta/Repository/DocumentationRepository.java
+++ src/main/java/com/upiicsa/ApiSIIP_Beta/Repository/DocumentationRepository.java
@@ -4,6 +4,10 @@ import com.upiicsa.ApiSIIP_Beta.Model.Documentation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DocumentationRepository extends JpaRepository<Documentation, Long> {
+
+    Optional<Documentation> findById(Long id);
 }
--- src/main/java/com/upiicsa/ApiSIIP_Beta/Service/DocumentationService.java
+++ src/main/java/com/upiicsa/ApiSIIP_Beta/Service/DocumentationService.java
@@ -1,13 +1,18 @@
 package com.upiicsa.ApiSIIP_Beta.Service;
 
+import com.upiicsa.ApiSIIP_Beta.Dto.Document.DocumentShowDto;
 import com.upiicsa.ApiSIIP_Beta.Model.Documentation;
 import com.upiicsa.ApiSIIP_Beta.Model.Enum.StateDocumentation;
 import com.upiicsa.ApiSIIP_Beta.Model.Student;
 import com.upiicsa.ApiSIIP_Beta.Repository.DocumentationRepository;
+import com.upiicsa.ApiSIIP_Beta.Repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class DocumentationService {
@@ -15,11 +20,26 @@ public class DocumentationService {
     @Autowired
     private DocumentationRepository documentationRepository;
 
+    @Autowired
+    private StudentRepository studentRepository;
+
     public Documentation createInitialDocumentation(Student student) {
         Documentation documentation = new Documentation();
         documentation.setStudent(student);
         documentation.setCreatedDate(LocalDateTime.now());
         documentation.setState(StateDocumentation.INCOMPLETA);
         return documentation;
+    }
+
+    public List<DocumentShowDto> getDocumentation(Long studentId) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        Documentation documentation = documentationRepository.findById(student.getDocumentation().getId())
+                .orElseThrow(() -> new UsernameNotFoundException("Documentation not found"));
+
+        return documentation.getDocuments().stream()
+                .map(d -> new DocumentShowDto(d))
+                .collect(Collectors.toList());
     }
 }
