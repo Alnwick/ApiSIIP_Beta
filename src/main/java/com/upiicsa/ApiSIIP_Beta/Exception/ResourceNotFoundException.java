5d8d04c3f32fe3e43737db57fdd91e358c164852
Alnwick <Alnwick0809@outlook.com>
2025-11-11 18:48:57 -0600

    Clase para cachar errores de entidades o elementos no encontrados.

--- src/main/java/com/upiicsa/ApiSIIP_Beta/Exception/ResourceNotFoundException.java
+++ src/main/java/com/upiicsa/ApiSIIP_Beta/Exception/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package com.upiicsa.ApiSIIP_Beta.Exception;
 
 public class ResourceNotFoundException extends RuntimeException {
-  public ResourceNotFoundException(String message) {
-    super(message);
-  }
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
