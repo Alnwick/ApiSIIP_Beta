1640ef33e7dd453fa45c9f7a8566d6de8c2a3b13
Alnwick <Alnwick0809@outlook.com>
2025-11-11 18:48:09 -0600

    Clase para cachar errores de validacion de DTO's.

--- src/main/java/com/upiicsa/ApiSIIP_Beta/Exception/ValidationException.java
+++ src/main/java/com/upiicsa/ApiSIIP_Beta/Exception/ValidationException.java
@@ -1,7 +1,7 @@
 package com.upiicsa.ApiSIIP_Beta.Exception;
 
 public class ValidationException extends RuntimeException {
-  public ValidationException(String message) {
-    super(message);
-  }
+    public ValidationException(String message) {
+        super(message);
+    }
 }
