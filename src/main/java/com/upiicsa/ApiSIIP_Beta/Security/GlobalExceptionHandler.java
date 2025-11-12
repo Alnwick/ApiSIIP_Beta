d71f44cd87d0c988ad4bd24d91e2ae0512f913b0
Alnwick <Alnwick0809@outlook.com>
2025-11-11 18:46:39 -0600

    Clase para cachar errores de BAD_REQUEST, NOT_FOUND y INTERNAL_SERVER

--- src/main/java/com/upiicsa/ApiSIIP_Beta/Security/GlobalExceptionHandler.java
+++ src/main/java/com/upiicsa/ApiSIIP_Beta/Security/GlobalExceptionHandler.java
@@ -1,4 +1,43 @@
 package com.upiicsa.ApiSIIP_Beta.Security;
 
+import com.upiicsa.ApiSIIP_Beta.Exception.ResourceNotFoundException;
+import com.upiicsa.ApiSIIP_Beta.Exception.ValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<String> handleValidationException(ValidationException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return new ResponseEntity<>("Error interno del sistema", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
